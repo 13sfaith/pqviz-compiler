584f3b8777642736da1c9a0025f786b099e85223
Spencer Faith <13sfaith@gmail.com>
2025-04-23 21:07:09 +0800

    basic i/o file testing

--- test/instrumentation/function-start.test.js
+++ test/instrumentation/function-start.test.js
@@ -1,6 +1,8 @@
 import plugin from '../../instrumentation/function-start.js'
 import { expect, test } from 'vitest'
-import babel from "@babel/core";
+import babel from "@babel/core"
+import path from 'path'
+import fs from 'fs'
 
 function runInstrumentation(originalCode, fileName = "test.js") {
     var { code } = babel.transformSync(originalCode, {
@@ -10,8 +12,14 @@ function runInstrumentation(originalCode, fileName = "test.js") {
     return code;
 }
 
+function compareResults(codeExampleName) {
+    const originalCode = fs.readFileSync(path.join(process.cwd(), `test/instrumentation/inputs/${codeExampleName}.js`), "utf8");
+    const newCode = runInstrumentation(originalCode)
+    const expectedCode = fs.readFileSync(path.join(process.cwd(), `test/instrumentation/outputs/function-start/${codeExampleName}.js`), "utf8");
+    console.log(newCode)
+    expect(newCode).toBe(expectedCode)
+}
+
 test('Variable Declaration Remains unchanged', () => {
-    let originalCode = 'let arg = 20;'
-    let newCode = runInstrumentation(originalCode)
-    expect(newCode).toBe(originalCode)
+    compareResults('top-level-declarations')
 })
